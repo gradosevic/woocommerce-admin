122a915b2022a6647d31b50664341fe21337e415
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-06-02 10:15:32 -0600

    Export user preferences store name.

--- packages/data/src/index.js
+++ packages/data/src/index.js
@@ -5,5 +5,6 @@ export { useSettings } from './settings/use-settings';
 export { PLUGINS_STORE_NAME } from './plugins';
 export { withPluginsHydration } from './plugins/with-plugins-hydration';
 
+export { USER_STORE_NAME } from './user-preferences';
 export { withCurrentUserHydration } from './user-preferences/with-current-user-hydration';
 export { useUserPreferences } from './user-preferences/use-user-preferences';
